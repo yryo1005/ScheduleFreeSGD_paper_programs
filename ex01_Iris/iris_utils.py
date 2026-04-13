9b38f00764f6b5a7f96d356b646fa68a78842f33
yryo1005 <33744568+yryo1005@users.noreply.github.com>
2026-04-13 16:46:21 +0900

    0

---
+++ ex01_Iris/iris_utils.py
@@ -0,0 +1,179 @@
+import copy
+from tqdm import tqdm
+
+import sklearn.datasets as sk_datasets
+from sklearn.model_selection import train_test_split
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+
+import sys
+sys.path.append("../")
+from utils import *
+
+def load_data(batch_size = 16, test_size = 0.1, num_workers = 1, seed = 0):
+
+    class IrisDataset(Dataset):
+        def __init__(self, inputs, teacher_signals):
+            if len(inputs) != len(teacher_signals):
+                raise
+
+            self.inputs = inputs
+            self.teacher_signals = teacher_signals
+
+        def __len__(self):
+            return len(self.inputs)
+
+        def __getitem__(self, idx):
+            input = torch.tensor( self.inputs[idx], dtype = torch.float32 )
+            teacher_signal = torch.tensor(self.teacher_signals[idx], dtype = torch.int64)
+
+            return input, teacher_signal
+
+    def collate_fn(batch):
+        inputs = torch.stack( [B[0] for B in batch] )
+        teacher_signals = torch.stack( [B[1] for B in batch] )
+
+        return inputs, teacher_signals
+
+    iris = sk_datasets.load_iris()
+    inputs = iris.data
+    teacher_signals = iris.target
+
+    tmp = list(zip(inputs, teacher_signals))
+    train_tmp, test_tmp = train_test_split(tmp, test_size = test_size, random_state = seed)
+
+    generator = torch.Generator()
+    generator.manual_seed(seed)
+
+    train_inputs, train_teacher_signals = zip(*train_tmp)
+    train_dataset = IrisDataset(train_inputs, train_teacher_signals)
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size = batch_size,
+        collate_fn = collate_fn,
+        num_workers = num_workers,
+        shuffle = True,
+        generator = generator
+    )
+
+    test_inputs, test_teacher_signals = zip(*test_tmp)
+    test_dataset = IrisDataset(test_inputs, test_teacher_signals)
+    test_dataloader = DataLoader(
+        test_dataset,
+        batch_size = batch_size,
+        collate_fn = collate_fn,
+        num_workers = num_workers,
+        shuffle = False,
+    )
+
+    return train_dataloader, test_dataloader
+
+def load_model(seed = 0):
+
+    class MyModel(nn.Module):
+        def __init__(self):
+            super().__init__()
+
+            self.fc1 = nn.Linear(4, 3)
+
+        def forward(self, inputs):
+            Y = self.fc1(inputs)
+
+            return Y
+
+    set_seed(seed = seed)
+    model = MyModel()
+
+    return model
+
+def train(target_path, optimizer, optimizer_params,
+          epochs = 100, batch_size = 16, test_size = 0.1, num_workers = 1,
+          seed = 0, verbose = False):
+
+    initial_metrics_sum = {"loss": 0, "acc": 0}
+    metrics = [f"train_{M}" for M in initial_metrics_sum.keys()] + \
+              [f"test_{M}" for M in initial_metrics_sum.keys()]
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    def loss_func(output, target):
+        loss = nn.CrossEntropyLoss()(output, target)
+        return loss
+
+    def metric_func(output, target):
+        output = output.argmax(dim = 1)
+        acc = (output == target).float().mean()
+        return acc
+
+    def iteration(X_batch, T_batch, model, optimizer = None):
+        X_batch = X_batch.to(device)
+        T_batch = T_batch.to(device)
+
+        if optimizer is not None:
+            model.train()
+            if hasattr(optimizer, "train"): optimizer.train()
+
+            optimizer.zero_grad()
+            Y_batch = model(X_batch)
+            loss = loss_func(Y_batch, T_batch)
+            loss.backward()
+            optimizer.step()
+
+        else:
+            model.eval()
+            if hasattr(optimizer, "eval"): optimizer.eval()
+
+            with torch.no_grad():
+                Y_batch = model(X_batch)
+                loss = loss_func(Y_batch, T_batch)
+
+        acc = metric_func(Y_batch, T_batch)
+
+        return {"loss": loss.item(),
+                "acc": acc.item()}
+
+    def epoch(dataloader, model, optimizer = None):
+
+        metric_sum = copy.deepcopy(initial_metrics_sum)
+        num_samples = 0
+
+        if verbose: pb = tqdm(dataloader)
+        else: pb = dataloader
+
+        for X_batch, T_batch in pb:
+
+            metric_batch = iteration(X_batch, T_batch, model, optimizer)
+
+            for K in metric_batch.keys():
+                metric_sum[K] += metric_batch[K] * len(X_batch)
+            num_samples += len(X_batch)
+
+            if verbose: pb.set_postfix({K:V / num_samples for K, V in metric_sum.items()})
+
+        metric_avg = {K:V / num_samples for K, V in metric_sum.items()}
+
+        return metric_avg
+
+    train_dataloader, test_dataloader = load_data(batch_size = batch_size,
+                                                  test_size = test_size,
+                                                  num_workers = num_workers,
+                                                  seed = seed)
+    model = load_model(seed = seed).to(device)
+    optimizer = optimizer(model.parameters(), **optimizer_params)
+
+    logger = ResultLogger()
+    logger.set_names(*metrics)
+
+    for i in range(epochs):
+        if verbose: print(f"epoch; {i}")
+
+        if i == 0:
+            train_metric_avg = epoch(train_dataloader, model)
+        else:
+            train_metric_avg = epoch(train_dataloader, model, optimizer)
+        test_metric_avg = epoch(test_dataloader, model)
+
+        logger(*train_metric_avg.values(), *test_metric_avg.values())
+
+    logger.save(target_path)
---
+++ utils.py
@@ -0,0 +1,356 @@
+import os
+import json
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from itertools import product
+
+import torch
+
+class ResultLogger:
+    def __init__(self, target_path = None):
+        self.names = None
+        self.history = {}
+
+        if target_path:
+            self.load(target_path)
+
+    def set_names(self, *names):
+        if self.names:
+            raise
+
+        self.names = list(names)
+        for name in self.names:
+            if name not in self.history:
+                self.history[name] = []
+
+    def __call__(self, *values):
+        if self.names is None:
+            raise
+        if len(values) != len(self.names):
+            raise
+
+        for name, value in zip(self.names, values):
+            self.history[name].append(value)
+
+    def save(self, target_path):
+        with open(target_path, "w") as f:
+            json.dump(self.history, f, indent=4)
+
+    def load(self, target_path):
+        with open(target_path, "r") as f:
+            data = json.load(f)
+            self.history = data
+            self.names = list(data.keys())
+
+    def __getitem__(self, key):
+        return self.history.get(key, [])
+
+def set_seed(seed = 0):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def get_param_id(params):
+    """
+        params; パラメータ (ex {"lr": 0.01, "momentum": 0.9}
+    """
+    return "_".join([str(params[k]) for k in sorted(params.keys())])
+
+def grid_search(target_dir, optimizer, fixed_params, search_space, train,
+                epochs = 100, batch_size = 16, test_size = 0.1, num_workers = 1,
+                num_seed = 5, verbose = False):
+    """
+        target_dir; 保存先のディレクトリ
+        optimizer; torch.optim.Optimizer
+        fixed_params; 固定のハイパーパラメータ (ex {"dampening": 0, "weight_decay": 0, "nesterov": False}
+        search_space; ハイパーパラメータの探索空間 (ex {"lr": [0.1, 0.01], "momentum": [0.0, 0.9]}
+        train; 学習関数
+    """
+    os.makedirs(target_dir, exist_ok = True)
+    for PARAMS in product(*search_space.values()):
+
+        # 探索空間のパラメータ
+        params = {
+            K: P for K, P in zip(search_space.keys(), PARAMS)
+        }
+
+        for seed in range(num_seed):
+
+            param_id = get_param_id(params)
+            target_path = f"{target_dir}/{param_id}_{seed}.json"
+
+            # 最適化手法のパラメータ
+            optimizer_params = {
+                **params,
+                **fixed_params
+            }
+            if os.path.exists(target_path):
+                continue
+
+            print(target_path)
+            train(target_path = target_path,
+                  optimizer = optimizer,
+                  optimizer_params = optimizer_params,
+                  seed = seed,
+                  epochs = epochs,
+                  batch_size = batch_size,
+                  test_size = test_size,
+                  num_workers = num_workers,
+                  verbose = verbose
+                )
+
+        with open(f"{target_dir}/{param_id}_config.json", "w") as f:
+            json.dump(optimizer_params, f, indent = 4)
+
+def get_best_results(target_dir, search_space, target_metric = "test_acc", mode = "max", num_seed = 5):
+
+    best_results = dict()
+    for PARAMS in product(*search_space.values()):
+
+        # 探索空間のパラメータ
+        params = {
+            K: P for K, P in zip(search_space.keys(), PARAMS)
+        }
+
+        best_metric = None
+        for seed in range(num_seed):
+
+            param_id = get_param_id(params)
+            target_path = f"{target_dir}/{param_id}_{seed}.json"
+            logger = ResultLogger(target_path = target_path)
+
+            if best_metric is None:
+                best_metric = logger[target_metric][-1]
+            else:
+                if mode == "min":
+                    best_metric = min(best_metric, logger[target_metric][-1])
+                elif mode == "max":
+                    best_metric = max(best_metric, logger[target_metric][-1])
+            if best_metric == logger[target_metric][-1]:
+                best_results[param_id] = logger
+
+    return best_results
+
+def plot_best_training_results(target_dir, best_results, search_space, metrics, ):
+
+    fig = plt.figure(figsize = (10, 10))
+    for i, M in enumerate(metrics):
+        N = int(np.sqrt(len(metrics)))
+        ax = fig.add_subplot(N, N, i + 1)
+        for K, V in best_results.items():
+            ax.plot(V[M], label = f"{K}")
+        ax.set_xlabel("epoch")
+        ax.set_ylabel(M)
+        ax.set_title(M)
+        ax.grid()
+
+    tmp = ""
+    for K in sorted(search_space.keys()):
+        tmp += f"{K}_"
+    tmp = tmp[:-1]
+    fig.suptitle(tmp)
+
+    fig.legend(
+        labels=[K for K in best_results.keys()],
+        loc='upper center',
+        ncol=5,  # 横に並べる列数
+        bbox_to_anchor=(0.5, 1.05) # figure の外に配置
+    )
+
+    fig.savefig(f"{target_dir}/best.png", bbox_inches = "tight")
+    plt.show()
+
+def get_confidence_intervals(target_dir, search_space, metrics, num_seed = 5):
+    confidence_intervals = dict()
+
+    for PARAMS in product(*search_space.values()):
+
+        # 探索空間のパラメータ
+        params = {
+            K: P for K, P in zip(search_space.keys(), PARAMS)
+        }
+
+        histories = {M: list() for M in metrics}
+        for seed in range(num_seed):
+            param_id = get_param_id(params)
+            target_path = f"{target_dir}/{param_id}_{seed}.json"
+            logger = ResultLogger(target_path = target_path)
+            for M in metrics:
+                histories[M].append(logger[M])
+            confidence_intervals[param_id] = histories
+
+        confidence_intervals[param_id] = {
+            M: {
+                "mean": np.array(histories[M]).mean(axis = 0),
+                "std": np.array(histories[M]).std(axis = 0)
+            }
+            for M in metrics
+        }
+
+    return confidence_intervals
+
+def plot_confidence_intervals(target_dir, confidence_intervals, metrics, ):
+
+    for K, V in confidence_intervals.items():
+        fig = plt.figure(figsize = (10, 10))
+        for i, M in enumerate(metrics):
+            N = int(np.sqrt(len(metrics)))
+            ax = fig.add_subplot(N, N, i + 1)
+
+            ax.plot(V[M]["mean"], label = f"{V[M]['mean'][-1]:.4f} ± {V[M]['std'][-1]:.4f}")
+            ax.fill_between(
+                x = range(len(V[M]["mean"])),
+                y1 = V[M]["mean"] - V[M]["std"],
+                y2 = V[M]["mean"] + V[M]["std"],
+                alpha=0.2,
+            )
+
+            ax.set_xlabel("epoch")
+            ax.set_ylabel(M)
+            ax.set_title(M)
+            ax.legend()
+            ax.grid()
+        fig.suptitle(K)
+        fig.savefig(f"{target_dir}/{K}_confidence_intervals.png", bbox_inches = "tight")
+
+        plt.show()
+
+def run(target_dir, optimizer, fixed_params, search_space, train,
+        epochs = 100, batch_size = 16, test_size = 0.1, num_workers = 1,
+        num_seed = 5, verbose = False,
+        metrics = ["train_loss", "train_acc", "test_loss", "test_acc"], target_metric = "test_acc", mode = "max"):
+    """
+        target_dir; 保存先のディレクトリ
+        optimizer; torch.optim.Optimizer
+        fixed_params; 固定のハイパーパラメータ (ex {"dampening": 0, "weight_decay": 0, "nesterov": False}
+        search_space; ハイパーパラメータの探索空間 (ex {"lr": [0.1, 0.01], "momentum": [0.0, 0.9]}
+        train; 学習関数
+    """
+
+    grid_search(target_dir = target_dir,
+                optimizer = optimizer,
+                fixed_params = fixed_params,
+                search_space = search_space,
+                train = train,
+                verbose = verbose)
+
+    best_results = get_best_results(target_dir = target_dir,
+                                    search_space = search_space,
+                                    target_metric = target_metric,
+                                    mode = mode,
+                                    num_seed = num_seed)
+
+    plot_best_training_results(target_dir = target_dir,
+                               best_results = best_results,
+                               search_space = search_space,
+                               metrics = metrics)
+
+    confidence_intervals = get_confidence_intervals(target_dir = target_dir,
+                                                    search_space = search_space,
+                                                    metrics = metrics,
+                                                    num_seed = num_seed)
+
+    plot_confidence_intervals(target_dir = target_dir,
+                              confidence_intervals = confidence_intervals,
+                              metrics = metrics)
+
+    training_results = dict()
+    for K in confidence_intervals.keys():
+        training_results[K] = dict()
+        for M in confidence_intervals[K].keys():
+            training_results[K][M] = {
+                "best": best_results[K][M],
+                "mean": confidence_intervals[K][M]["mean"].tolist(),
+                "std": confidence_intervals[K][M]["std"].tolist()
+            }
+
+        with open(f"{target_dir}/{K}.json", "w") as f:
+            json.dump(training_results[K], f, indent = 4)
+
+    with open(f"{target_dir}/training_results.json", "w") as f:
+        json.dump(training_results, f, indent=4)
+
+def run_all(root_dir, optimizer_to_params, train,
+            epochs, batch_size, test_size, num_workers, num_seed, verbose,
+            metrics = ["train_loss", "train_acc", "test_loss", "test_acc"], target_metric = "test_acc", mode = "max"):
+    """
+        target_dir; 保存先のディレクトリ
+        optimizer; torch.optim.Optimizer
+        fixed_params; 固定のハイパーパラメータ (ex {"dampening": 0, "weight_decay": 0, "nesterov": False}
+        search_space; ハイパーパラメータの探索空間 (ex {"lr": [0.1, 0.01], "momentum": [0.0, 0.9]}
+        train; 学習関数
+    """
+
+    root_dir = f"{root_dir}/{epochs}_{batch_size}_{test_size}"
+    os.makedirs(root_dir, exist_ok = True)
+
+    optimizer_to_results = dict()
+
+    for K, V in optimizer_to_params.items():
+        target_dir = f"{root_dir}/{K}"
+
+        run(target_dir = target_dir,
+            optimizer = V["optimizer"],
+            fixed_params = V["fixed_params"],
+            search_space = V["search_space"],
+            train = train,
+            epochs = epochs,
+            batch_size = batch_size,
+            test_size = test_size,
+            num_workers = num_workers,
+            num_seed = num_seed,
+            verbose = verbose,
+            metrics = metrics,
+            target_metric = target_metric,
+            mode = mode)
+
+        with open(f"{target_dir}/training_results.json", "r") as f:
+            optimizer_to_results[K] = json.load(f)
+
+    optimizer_to_best_results = dict()
+    for K in optimizer_to_params.keys():
+        with open(f"{root_dir}/{K}/training_results.json", "r") as f:
+            results = json.load(f)
+
+        best = None
+        for KK, VV in results.items():
+            if best == None:
+                best = {"params": KK, **VV}
+            else:
+                if mode == "max":
+                    if best[target_metric]["mean"][-1] < VV[target_metric]["mean"][-1]:
+                        best = {"params": KK, **VV}
+                elif mode == "min":
+                    if best[target_metric]["mean"][-1] > VV[target_metric]["mean"][-1]:
+                        best = {"params": KK, **VV}
+
+        tmp = f"{K}_{best['params']}"
+        optimizer_to_best_results[tmp] = best
+
+    fig = plt.figure(figsize = (10, 10))
+    for i, M in enumerate(metrics):
+        ax = fig.add_subplot(int(np.sqrt(len(metrics))), int(np.sqrt(len(metrics))), i + 1)
+
+        for K, V in optimizer_to_best_results.items():
+
+            ax.plot(V[M]["mean"], label = f"{K} ± {V[M]['std'][-1]:.4f}")
+            ax.fill_between(
+                x = range(len(V[M]["mean"])),
+                y1 = np.array(V[M]["mean"]) - np.array(V[M]["std"]),
+                y2 = np.array(V[M]["mean"]) + np.array(V[M]["std"]),
+                alpha=0.2,
+            )
+
+        ax.set_xlabel("epoch")
+        ax.set_ylabel(M)
+        ax.set_title(M)
+        ax.legend()
+        ax.grid()
+
+    fig.savefig(f"{root_dir}/best_confidence_intervals.png", bbox_inches = "tight")
+    plt.show()
